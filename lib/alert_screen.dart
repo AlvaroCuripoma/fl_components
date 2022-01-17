583b5a8ae6ab5686c33edfcdc0ae294adfe0948f
Curipoma <alvarocuripoma@gmail.com>
2022-01-17 13:58:42 -0500

    sadasds

--- lib/alert_screen.dart
+++ lib/alert_screen.dart
@@ -1,43 +1,82 @@
 import 'dart:io';
 
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class AlertScreen extends StatelessWidget {
   const AlertScreen({Key? key}) : super(key: key);
 
-  void displayDialog(BuildContext context) {
+  void displayDialogIos(BuildContext context) {
+    showCupertinoDialog(
+      barrierDismissible: true,
+      context: context,
+      builder: (context) => CupertinoAlertDialog(
+        title: const Text('Titulo'),
+        content: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: const [
+            Text('Este es el contenido de la alerta'),
+            SizedBox(
+              height: 10,
+            ),
+            FlutterLogo(
+              size: 100,
+            )
+          ],
+        ),
+        actions: [
+          TextButton(
+              onPressed: () => Navigator.pop(context),
+              child: const Text(
+                'Cancelar',
+                style: TextStyle(color: Colors.red),
+              )),
+          TextButton(
+            onPressed: () => Navigator.pop(context),
+            child: const Text('Ok'),
+          ),
+        ],
+      ),
+    );
+  }
+
+  void displayDialogAndroid(BuildContext context) {
     showDialog(
-        barrierDismissible: true,
-        context: context,
-        builder: (context) {
-          return AlertDialog(
-            elevation: 5,
-            title: const Text('Titulo'),
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadiusDirectional.circular(15),
-            ),
-            content: Column(
-              mainAxisSize: MainAxisSize.min,
-              children: const [
-                Text('Este es el contenido de la alerta'),
-                SizedBox(
-                  height: 10,
-                ),
-                FlutterLogo(
-                  size: 50,
-                )
-              ],
-            ),
-            actions: [
-              TextButton(
-                  onPressed: () => Navigator.pop(context),
-                  child: const Text('Cerrar')),
-              TextButton(
-                  onPressed: () => Navigator.pop(context),
-                  child: const Text('Cerrar'))
+      barrierDismissible: true,
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          elevation: 5,
+          title: const Text('Titulo'),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadiusDirectional.circular(15),
+          ),
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: const [
+              Text('Este es el contenido de la alerta'),
+              SizedBox(
+                height: 10,
+              ),
+              FlutterLogo(
+                size: 100,
+              )
             ],
-          );
-        });
+          ),
+          actions: [
+            TextButton(
+                onPressed: () => Navigator.pop(context),
+                child: const Text(
+                  'Cancelar',
+                  style: TextStyle(color: Colors.red),
+                )),
+            TextButton(
+                onPressed: () => Navigator.pop(context),
+                child: const Text('Ok'))
+          ],
+        );
+      },
+    );
   }
 
   @override
@@ -45,6 +84,7 @@ class AlertScreen extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Alert Screen'),
+        actions: [],
         elevation: 0,
       ),
       body: Center(
@@ -56,12 +96,9 @@ class AlertScreen extends StatelessWidget {
               style: TextStyle(fontSize: 16),
             ),
           ),
-          onPressed: () {
-            // Navigator.pop(context);
-            // displayDialog(context);
-            Platform.isAndroid;
-            // ?
-          },
+          onPressed: () => Platform.isAndroid
+              ? displayDialogAndroid(context)
+              : displayDialogIos(context),
         ),
       ),
       floatingActionButton: FloatingActionButton(
